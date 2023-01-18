e3b1b4a3331b94d50b235f09f62a1c4d4f583bdc
itzish1999 <ismaeel.butt19@gmail.com>
2023-01-18 17:09:48 -0500

    feat(.env): added a .env file w instructions

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const { PORT } = require("../config");
 
 const app = express();
 
-const PORT = 1000;
-
 app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+    res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
     console.log(`App Is Listening on PORT ${PORT}`)
-})
+});
